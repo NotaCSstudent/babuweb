797543b415fbf23326a4a2c77561204104271c92
Mahamud Babu <mahmudbabu94@gmail.com>
2020-05-14 20:04:32 -0400

    Updates

---
+++ precache-manifest.f768e98c700ce91f8f2204a7e029bfe6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06937f5f5d696d10eaf1ceb08b384bc8",
+    "url": "//mbabu94.github.io/babuweb/index.html"
+  },
+  {
+    "revision": "580cd92c60df7ed35dca",
+    "url": "//mbabu94.github.io/babuweb/static/css/main.022c4c98.chunk.css"
+  },
+  {
+    "revision": "4c9383906e54fa145af8",
+    "url": "//mbabu94.github.io/babuweb/static/js/2.cd449228.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//mbabu94.github.io/babuweb/static/js/2.cd449228.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "580cd92c60df7ed35dca",
+    "url": "//mbabu94.github.io/babuweb/static/js/main.1fa14d68.chunk.js"
+  },
+  {
+    "revision": "948d398d18a9e41190a8",
+    "url": "//mbabu94.github.io/babuweb/static/js/runtime-main.68fcd174.js"
+  },
+  {
+    "revision": "229db538555e86290abcba61467473d1",
+    "url": "//mbabu94.github.io/babuweb/static/media/babupic.229db538.jpeg"
+  }
+]);
