e5058020eea4b91706d7adafcc0f8fd38ead0d90
mahamud babu <mahmudbabu94@gmail.com>
2021-01-21 18:23:27 -0500

    shoutout to cuny hack

--- src/Babupage/Welcome.js
+++ src/Babupage/Welcome.js
@@ -13,7 +13,7 @@ export default class Welcome extends Component{
   render(){
 
     
-    const a = (name = "BAN EMBEE") =>{
+    const a = (name = "SHOUT OUT TO CUNY HACKATHON") =>{
       const g = name;
       return g;
     }
